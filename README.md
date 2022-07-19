ffd37e7adf9b01070d853bdf615e49b8f6e0d1f0
Arshad Majgaonkar <arshad.majgaonkar@gep.com>
2022-07-19 13:45:11 +0530

    frontend + backend : completed. Readme : Updated

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
+
+
 # AngularTodoApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.5.
+
+## About the project
+
+Don't MEMORISE ! Here comes a simple todo list app which you can use to track your daily tasks further increasing your daily productive
+
+![](src/assets/img/output.png)
+
 
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
 
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
 
 ## Build
 
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -10,27 +10,45 @@ export class TodosComponent implements OnInit {
 
   todos:Todo[]=[];
   inputTodo:string;
+  removeBtn;
 
   constructor() {
-    console.log("i am constructor of todos.component");
+    // console.log("i am constructor of todos.component");
     
     this.todos=[
-      new Todo(1,"purchase a bat",false),
-      new Todo(2,"purchase glows",false),
-      new Todo(3,"purchase sports shoes",false)
+      new Todo(0,"Purchase a bat",false),
+      new Todo(1,"Purchase gloves",false),
+      new Todo(2,"Purchase sports shoes",false)
     ];
    }
 
   ngOnInit(): void {
-    console.log("i am ngOnInit");
+    // console.log("i am ngOnInit");
     
   }
 
   add()
   {
-    console.log("i am add method");
     
     this.todos.push(new Todo(this.todos.length,this.inputTodo,false));
     this.inputTodo="";
+  }
+
+  newTodoList:Todo[]=[];
+
+  remove(index,length)
+  {
+    for(let i=0; i<length; i++)
+    {
+      if(i!=index)
+      this.newTodoList.push(this.todos[i]);
+    }
+
+    this.todos = this.newTodoList;
+
+    this.newTodoList=[];
+    // this.todos = this.todos.filter(todo => todo.id!=index);
+    
+    
   }
 }
